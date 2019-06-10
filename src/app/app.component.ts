d75a18b58dc7b29e2324151a09117ecd8f58a761
orinera19 <ori_nera@hotmail.com>
2019-06-10 16:48:38 +0700

    minnie
    huh

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+declare var UIkit: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Project';
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
+}
